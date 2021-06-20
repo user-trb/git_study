f477c696e2e77f9e81c46b6beb0762d7f79a8b5c
tianrb <2754686579@qq.com>
2021-06-20 16:21:43 +0800

    添加app文件

---
+++ app/admin/model/admin/ModelAdminMenu.php
@@ -0,0 +1,9 @@
+<?php
+namespace app\admin;
+
+use think\Model;
+
+class ModelAdminMenu extends Model
+{
+
+}
---
+++ app/index/controller/Index.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\index\controller;
+
+use app\BaseController;
+use think\facade\View;
+
+class Index extends BaseController
+{
+    public function index()
+    {
+        return View::fetch();
+    }
+}
